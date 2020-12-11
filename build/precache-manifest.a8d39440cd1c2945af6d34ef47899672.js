1084ef0e0d82a9c0d55276716ab40e141bbf0304
s.sajadi <s.sajadi@ghoghnos.net>
2020-12-11 15:08:22 -0500

    updating

---
+++ build/precache-manifest.a8d39440cd1c2945af6d34ef47899672.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fe6ab3297e1c6c386b1520cd5dcd27d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1581f83e4ba5b002292",
+    "url": "/static/css/2.1646c65d.chunk.css"
+  },
+  {
+    "revision": "db96dd791de0f5d83766",
+    "url": "/static/css/3.b6b42a09.chunk.css"
+  },
+  {
+    "revision": "ca307040f7733ccf737b",
+    "url": "/static/css/main.79dc2665.chunk.css"
+  },
+  {
+    "revision": "b1581f83e4ba5b002292",
+    "url": "/static/js/2.21bb0a53.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.21bb0a53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db96dd791de0f5d83766",
+    "url": "/static/js/3.f64898ec.chunk.js"
+  },
+  {
+    "revision": "ca307040f7733ccf737b",
+    "url": "/static/js/main.61d021a6.chunk.js"
+  },
+  {
+    "revision": "6fa94ba8a1c52bc1794d",
+    "url": "/static/js/runtime-main.b100e1f2.js"
+  },
+  {
+    "revision": "81385fef488ec03f3ebd2e3165c29210",
+    "url": "/static/media/food.81385fef.jpg"
+  },
+  {
+    "revision": "943f9d466464306db7971e00836214a0",
+    "url": "/static/media/footer.943f9d46.svg"
+  },
+  {
+    "revision": "9e778b6d14e53fcf855f3196e87ca772",
+    "url": "/static/media/opeqe-logo.9e778b6d.svg"
+  },
+  {
+    "revision": "0d61a544f5691fb4ba4b4305316ea3fe",
+    "url": "/static/media/slider.0d61a544.jpg"
+  }
+]);
